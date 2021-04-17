a55393b95b24b08d174bf6f47ee8b4eac5198ff8
Gabriela Janiak <gabrysia_janiak@o2.pl>
2021-04-17 23:06:16 +0200

    Contact form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,45 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import "../scss/_contact.scss";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const Contact = () => {
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Imię</Form.Label>
-        <Form.Control type="text" placeholder="Imię" />
-      </Form.Group>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Email" />
+    <Form className="form-container">
+      <Form.Group controlId="formBasicText" className="input-row">
+        <Form.Label className="label">Imię</Form.Label>
+        <Form.Control type="text" placeholder="Imię" className="input" />
       </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="text" placeholder="Twoja wiadomość" />
+      <Form.Group controlId="formBasicText" className="input-row">
+        <Form.Label className="label">Nazwisko</Form.Label>
+        <Form.Control type="text" placeholder="Nazwisko" className="input" />
       </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+
+      <Form.Group controlId="exampleForm.ControlInput1" className="input-row">
+        <Form.Label className="label">E-mail</Form.Label>
+        <Form.Control type="email" placeholder="E-mail" className="input" />
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+
+      <Form.Group
+        controlId="exampleForm.ControlTextarea1"
+        className="input-row"
+      >
+        <Form.Label className="label">Wiadomość</Form.Label>
+        <Form.Control as="textarea" rows={3} className="input" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicCheckbox" className="input-row">
+        <Form.Check
+          type="checkbox"
+          label="Zapisz się do newslettera"
+          className="checkbox"
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" className="button">
+        Wyślij
       </Button>
     </Form>
   );
